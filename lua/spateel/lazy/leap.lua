21948242dc395a5b88211919fd25ce0914dec1d3
sunil-pateel <spateel@caldrix.com>
2025-04-10 23:53:45 -0400

    Got rid of leap

--- lua/spateel/lazy/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ggandor/leap.nvim",
-    keys = {{"s", mode = "n"},{"s", mode = "v"}, {"S", mode = "n"}, {"S", mode = "v"}},
-
-    config = function()
-        require('leap').create_default_mappings()
-    end
-}
-
