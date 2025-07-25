ac90d453bcbd53399272d2179c83ae3d1929748d
= <=>
2025-07-25 16:55:58 -0400

    fix: border

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,44 +1,31 @@
 return {
     'saghen/blink.cmp',
 
-    -- use a release tag to download pre-built binaries
     version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'enter' },
+        keymap = {
+            preset = 'enter',
+            ['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' }
+        },
 
         appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = 'mono'
         },
 
-        -- (Default) Only show the documentation popup when manually triggered
         completion = {
-            documentation = { auto_show = true },
+            documentation = {
+                auto_show = true,
+                window = {
+                    border = "rounded"
+                }
+            },
+            menu = {
+                border = "rounded"
+            }
         },
-
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
@@ -48,8 +35,19 @@ return {
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "lua" },
-        signature = { enabled = true }
+        fuzzy = {
+            implementation = "prefer_rust",
+            prebuilt_binaries = {
+                force_version = "1.6",
+            }
+        },
+        signature = {
+            enabled = true,
+            window = {
+                border = "rounded"
+            }
+        }
     },
+
     opts_extend = { "sources.default" }
 }
